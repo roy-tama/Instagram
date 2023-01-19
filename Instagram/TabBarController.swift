c8b1a5878eb02d4a4d4b4ed767a7014123a628a7
tatsu.maki <tatsuya.spring0414@gmail.com>
2023-01-19 12:38:24 +0900

    lesson8 4まで完了

---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,55 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 牧野達也 on 2023/01/19.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // タブの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        
+        // タブバーの背景色を設定
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        self.tabBar.standardAppearance = appearance
+        self.tabBar.scrollEdgeAppearance = appearance
+        
+        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する
+        self.delegate = self
+    }
+    
+    //タブバーのアイコンがタップされた時に呼ばれるDelegateメソッドを処理する
+    // true: タブ切替許可
+    // false: タブ切替不許可
+    func tabBarController(_ tabBarController:UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is ImageSelectViewController {
+            // ImageSelectViewControllerはタブ切り替えではなくモーダル画面遷移をする
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+            present(imageSelectViewController, animated: true)
+            return false
+        } else {
+            // その他のViewControllerは通常のタブ切り替えを実施
+            return true
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
